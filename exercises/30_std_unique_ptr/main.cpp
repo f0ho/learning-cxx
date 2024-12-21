33fe4eb373c44ddcb39750e70eae95128ea20931
f0ho <123@example.com>
2024-12-21 18:32:15 +0800

    b

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -58,18 +58,20 @@ int main(int argc, char **argv) {
     std::vector<const char *> answers[]{
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        {"d"},
-        {"ffr"},
+        {"d", "ffr"},
+        {"r", "d", "d"},
     };
 
     // ---- 不要修改以下代码 ----
-
+    
     for (auto i = 0; i < 3; ++i) {
-        ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
             std::cout << problems[i][j].c_str() << " ";
             std::cout << std::endl;
         }
+    }
+    for (auto i = 0; i < 3; ++i) {
+        ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
